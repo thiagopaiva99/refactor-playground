16c1ac96032df9f57f8f0fd46965286bb9cb9d62
Thiago Galvani <thiagopaiva99@gmail.com>
2019-04-27 22:38:31 -0300

    Refactoring code and creating more examples

--- is-prime/README.md
+++ is-prime/README.md
@@ -29,39 +29,4 @@ console.log(isPrime(10007)) // true
 
 ```
 
-`Refactored` code:
-```
-const isSquareRootAnIntegerNumber = number => 
-    Number.isInteger(Math.sqrt(number))
-
-const hasZeroModBy = value => number => 
-    number % value === 0
-
-const isEvenAndDiferentFrom = value => number => 
-    hasZeroModBy(2)(number) && number !== value
-
-const isEvenAndDiferentFromTwoAndHasIntegerSquareRoot = number =>
-    isEvenAndDiferentFrom(2)(number) || isSquareRootAnIntegerNumber(number)
-
-const getHalfValueOf = number => 
-    Math.floor(number / 2)
-
-const isPrime = number => {
-  if (isEvenAndDiferentFromTwoAndHasIntegerSquareRoot(number)) 
-    return false
-
-  const initialIterationValue = getHalfValueOf(number)
-
-  for (let i = initialIterationValue; i >= 2; i--) {
-    if (hasZeroModBy(i)(number)) {
-      return false
-    }
-  }
-
-  return true
-}
-
-console.log(isPrime(10002)) // false
-console.log(isPrime(10003)) // false
-console.log(isPrime(10007)) // true
-```
+`Refactored` code is at `index.js`
--- is-prime/index.js
+++ is-prime/index.js
@@ -1,33 +1,39 @@
+const not = condition =>
+  !condition
+
 const isSquareRootAnIntegerNumber = number => 
-    Number.isInteger(Math.sqrt(number))
+  Number.isInteger(Math.sqrt(number))
 
 const hasZeroModBy = value => number => 
-    number % value === 0
+  number % value === 0
 
 const isEvenAndDiferentFrom = value => number => 
-    hasZeroModBy(2)(number) && number !== value
+  hasZeroModBy(2)(number) && number !== value
 
 const isEvenAndDiferentFromTwoAndHasIntegerSquareRoot = number =>
-    isEvenAndDiferentFrom(2)(number) || isSquareRootAnIntegerNumber(number)
+  isEvenAndDiferentFrom(2)(number) || isSquareRootAnIntegerNumber(number)
 
 const getHalfValueOf = number => 
-    Math.floor(number / 2)
+  Math.floor(number / 2)
 
-const isPrime = number => {
-  if (isEvenAndDiferentFromTwoAndHasIntegerSquareRoot(number)) 
-    return false
-
+const hasDivisor = number => {
   const initialIterationValue = getHalfValueOf(number)
 
   for (let i = initialIterationValue; i >= 2; i--) {
-    if (hasZeroModBy(i)(number)) {
-      return false
-    }
+    if (hasZeroModBy(i)(number))
+      return true
   }
 
-  return true
+  return false
 }
 
-console.log(isPrime(10002)) // false
-console.log(isPrime(10003)) // false
-console.log(isPrime(10007)) // true
+const isPrime = number => 
+  (not(isEvenAndDiferentFrom(2)(number)) && 
+   not(isSquareRootAnIntegerNumber(number)) && 
+   not(hasDivisor(number)))
+
+const numbers = [...Array(20).keys()]
+
+const primeNumbers = numbers.filter(isPrime)
+
+console.log(primeNumbers)
